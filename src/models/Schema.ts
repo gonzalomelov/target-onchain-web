7c4e203dd33d765fc678e3863c24645d4cd778e7
Gonzalo Melo <gonzalomelov@gmail.com>
2024-07-20 07:33:54 -0300

    chore: remove frame.scan

--- src/models/Schema.ts
+++ src/models/Schema.ts
@@ -41,7 +41,6 @@ export const frameSchema = mysqlTable('Frame', {
   productHandle: varchar('productHandle', { length: 255 }).notNull(),
   productVariantId: varchar('productVariantId', { length: 255 }).notNull(),
   destination: varchar('destination', { length: 255 }).notNull(),
-  scans: int('scans').default(0).notNull(),
   createdAt: int('createdAt').default(0).notNull(),
   updatedAt: int('updatedAt').default(0).notNull(),
   image: varchar('image', { length: 255 }).notNull(),
