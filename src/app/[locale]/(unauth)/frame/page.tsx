5d8b39f3ec9618f8cd95bba75e883e938e8560d3
Gonzalo Melo <gonzalomelov@gmail.com>
2024-07-22 06:06:50 -0300

    chore: remove unused components

--- src/app/[locale]/(unauth)/frame/page.tsx
+++ src/app/[locale]/(unauth)/frame/page.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import { useTranslations } from 'next-intl';
 import { getTranslations } from 'next-intl/server';
 import { Suspense } from 'react';
@@ -28,30 +27,6 @@ const Frame = () => {
       <Suspense fallback={<p>{t('loading_frame')}</p>}>
         <FrameList />
       </Suspense>
-
-      <div className="mt-2 text-center text-sm">
-        {`${t('error_reporting_powered_by')} `}
-        <a
-          className="text-blue-700 hover:border-b-2 hover:border-blue-700"
-          href="https://sentry.io/for/nextjs/?utm_source=github&utm_medium=paid-community&utm_campaign=general-fy25q1-nextjs&utm_content=github-banner-nextjsboilerplate-logo"
-          target="_blank"
-        >
-          Sentry
-        </a>
-      </div>
-
-      <a
-        href="https://sentry.io/for/nextjs/?utm_source=github&utm_medium=paid-community&utm_campaign=general-fy25q1-nextjs&utm_content=github-banner-nextjsboilerplate-logo"
-        target="_blank"
-      >
-        <Image
-          className="mx-auto mt-2"
-          src="/assets/images/sentry-dark.png"
-          alt="Sentry"
-          width={130}
-          height={112}
-        />
-      </a>
     </>
   );
 };
